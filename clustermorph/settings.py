105e669bef42d8266da51e6152b3ad57789b3064
Wallace-p-p <wallacepncp@gmail.com>
2022-09-03 15:55:52 -0300

    make better

--- clustermorph/settings.py
+++ clustermorph/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ih@2jfucr)_*xgb@eomu(h+uld11yrispv83(v1&0iu0%3v^o%'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production!.
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
